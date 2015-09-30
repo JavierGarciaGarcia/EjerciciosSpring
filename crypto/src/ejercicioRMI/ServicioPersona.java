c267cc2325ef0416d5582b0221f2bca0357930ea
jaggarcia <jaggarcia@malsalaform01W7.indra.es>
2015-09-30 12:05:32 +0200

    Cambio para probar

--- crypto/src/ejercicioRMI/ServicioPersona.java
+++ crypto/src/ejercicioRMI/ServicioPersona.java
@@ -14,6 +14,7 @@ import javax.rmi.ssl.SslRMIServerSocketFactory;
 
 /**
  * @author jaggarcia
+ * Servicio encargado de trabajar con la entidad persona
  *
  */
 public class ServicioPersona extends UnicastRemoteObject implements IServicioPersonas {
